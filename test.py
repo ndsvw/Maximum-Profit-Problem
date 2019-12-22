cc0110bca1f302946703e714fa017a92aa526208
Alex Lübeck <ndsvw@gmx.de>
2019-12-22 21:57:32 +0100

    unit test for special cases of k

--- test.py
+++ test.py
@@ -27,6 +27,22 @@ class MaxProfitTest(unittest.TestCase):
         self.assert_limited_methods_result_equals_n(G, 1, 8)
         self.assert_limited_methods_result_equals_n(G, 10, 8)
 
+    def test_k_equals_zero(self):
+        G1 = [1, 8]
+        G2 = [1, 8, 3, 8]
+        G3 = [8, 10, 11, 4, 3]
+        self.assert_limited_methods_result_equals_n(G1, 0, 0)
+        self.assert_limited_methods_result_equals_n(G2, 0, 0)
+        self.assert_limited_methods_result_equals_n(G3, 0, 0)
+
+    def test_large_k(self):
+        G1 = [1, 8]
+        G2 = [1, 8, 3, 8]
+        G3 = [8, 10, 11, 4, 3]
+        self.assert_limited_methods_result_equals_n(G1, 100, 7)
+        self.assert_limited_methods_result_equals_n(G2, 100, 12)
+        self.assert_limited_methods_result_equals_n(G3, 100, 3)
+
 
 if __name__ == "__main__":
     unittest.main()
